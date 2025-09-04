f65f86083ff0f8c71f8992a2dfb224deb8224f4a
pastry <pastry@163.com>
2025-09-04 18:42:40 +0530

    add
    add

---
+++ MyLibraryTools/Sources/MyLibraryTools/MyLibraryTools.swift
@@ -0,0 +1,12 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+import UIKit
+
+class Tools: NSObject {
+    
+    func toolstr() -> String {
+        return "Tools"
+    }
+}
